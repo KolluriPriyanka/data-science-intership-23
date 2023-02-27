8b3d450e2f70a3a34bfb7f2da69b94140f5ec7db
Kolluri Priyanka <113817161+KolluriPriyanka@users.noreply.github.com>
2023-02-27 13:33:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("My First Assignment in Innomatics Data App :boom:")
+
+st.header("My Name is Junnu :heart:")
+
+st.subheader(" I am very glad to meet you all in this greatful platform! :heart_eyes:")
+
+
